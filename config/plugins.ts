9a8f3374c7f9172653ad687cf47e9d4499a8e9ae
Nathan Zilora <zwork101@gmail.com>
2025-03-14 16:51:54 -0400

    Added deployment plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,30 @@
-export default () => ({});
+module.exports = {
+  'strapi-plugin-deploy-dashboard': {
+    enabled: true,
+    config: {
+      deployments: [
+        {
+          name: "Feedback Website",
+          instances: [
+            {
+              id: "production",
+              name: "Production",
+              hookUrl: "https://api.github.com/repos/Zwork101/feedback-template/actions/workflows/aws.yml/dispatches",
+              requestOptions: {
+                method: "POST",
+                headers: {
+                  "Accept": "application/vnd.github+json",
+                  "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`,
+                  "X-GitHub-Api-Version": "2022-11-28"
+                },
+                body: {
+                  ref: "main"
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+ };
